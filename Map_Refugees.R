3a8c431e6986003778d3a0d2a173bd4cb956ade3
eugejoh <ejoh90@gmail.com>
2017-01-26 20:47:14 -0500

    Map_Refugees_jan26
    edits on 1/26/2017

--- Map_Refugees.R
+++ Map_Refugees.R
@@ -1,7 +1,9 @@
 # Refugee Data Visualization
 # 
-# https://data.unhcr.org/dataviz/#
+# https://data.unhcr.org/dataviz/ THE GOAL IS TO REPLICATE THIS IN SHINY
 
+# METADAT on dataset
+# http://popstats.unhcr.org/en/overview
 rm(list=ls())
 
 getwd() #home directory /Users/eugenejoh
@@ -47,7 +49,9 @@ table(ref.d$Year) #we see there is alot of missing data
 ref.na<-apply(ref.d,2, function(x) sum(is.na(x))) #counts the number of NAs in each column
 ref.na #counts of the NAs, row total is 103746
 
+# https://cran.r-project.org/doc/manuals/r-release/R-lang.html#Classes
 str(ref.d) #structure of the columns
+
 # two factors nad the  remainder are integers
 summary(ref.d$Year)
 ref.d$Country <- as.character(ref.d$Country) #convert from factor to string
@@ -62,17 +66,64 @@ for(i in 2:length(names(ref.d))){ # "2"-ignores the first column (we want to kee
 	}
 }
 
-ifelse(ref.d$Country_Origin=="Various/Unknown",ref.d$Country_Origin=="Unknown",ref.d$Country_Origin)
-
 ref.d$Country_Origin[ref.d$Country_Origin=="Various/Unknown"] <- "UNKNOWN" #change the "Various/Unknown" to a new string label "UNKNOWN" in the country of origin column
 ref.d$Country[ref.d$Country=="Various/Unknown"] <- "UNKNOWN" #do the same for the countries
 
-table(ref.d$Country)
-table(ref.d$Country_Origin)
+table(ref.d$Country) #check the above changes
+table(ref.d$Country_Origin) #check the above changes
+
+
+clist<-sort(unique(ref.d$Country)) #alphabetical list of countries in Country
+or.clist<-sort(unique(ref.d$Country_Origin)) #alphabetical list of countries in Country_Origin
+
+# We want to if there are any differences between these two vectors of country names
+# http://stackoverflow.com/questions/9162134/comparing-character-vectors-in-r-to-find-unique-and-or-missing-values
+clist[!clist %in% or.clist] #which countries from clist are not in or.clist
+ # this means that these countries either don't have data for refugees or there are no refugees in these countries
+ # this means that these countries have not produced refugees or there is no data on refugees from these countries
+setdiff(countries,or.countries) #also accomplishes the same as line 81
+
+or.clist[!or.clist %in% clist] #which countries from or.clist are not in clist
+ # this means that these are ONLY countries of origin and either there is no data that refugees seltted or there are no refugees in these countries
+ # this means that these countries (ie. NK) have only produced refugees (not taken in any refugees) or there is no data on this.
+setdiff(or.countries,countries) #also accomplishes the same as line 86
+
+ref.d[which(ref.d$Country=="Bonaire"),] #retrieves rows that match the criteria: country Bonaire
+head(ref.d[which(ref.d$Country_Origin=="Dem. People's Rep. of Korea"),]) #retrievs the rows that match the criteria: country of origin as NK
+
+
 apply(ref.d,2,function(x) ifelse(class(x)=="factor",as.numeric(x)))
 apply(ref.d[1:10,1:10],2,function(x) paste0(x,"asdf"))
+
+
+# Make a wordcloud to qualitatively visualize the countries that have taken in refugees
+library(wordcloud) #https://www.r-bloggers.com/word-cloud-in-r/
 countries<-sort(unique(ref.d$Country),decreasing=F)
 countries
+
+country.counts <- as.data.frame(table(ref.d$Country))
+pal2 <- brewer.pal(10,"Accent") #palette
+wordcloud(countries,
+	country.counts[,2],
+	scale=c(2.5,.2),
+	min.freq=10,
+	vfont=c("serif","bold"),
+	random.order=FALSE,
+	colors=pal2) # selection of font #ref: https://stat.ethz.ch/R-manual/R-devel/library/grDevices/html/Hershey.html
+#http://blog.revolutionanalytics.com/2012/09/how-to-use-your-favorite-fonts-in-r-charts.html
+
+# Make a worldcloud to ... refugee country of origin
+or.countries<-sort(unique(ref.d$Country_Origin),decreasing=F)
+or.countries
+
+or.country.counts <- as.data.frame(table(ref.d$Country_Origin))
+wordcloud(or.countries,
+	or.country.counts[,2],
+	scale=c(2.5,.2),
+	min.freq=10,
+	vfont=c("serif","bold"),
+	random.order=FALSE,
+	colors=pal2)
 
 for (i in )
 
