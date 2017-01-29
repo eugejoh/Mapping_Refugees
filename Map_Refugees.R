904cf9914b88f2d72c4a5dca62e11ce550e13f7b
eugejoh <ejoh90@gmail.com>
2017-01-28 20:08:10 -0500

    Map_Refugees_12817
    Troubleshoot plyr method of renaming levels

--- Map_Refugees.R
+++ Map_Refugees.R
@@ -52,8 +52,40 @@ ref.na #counts of the NAs, row total is 103746
 # https://cran.r-project.org/doc/manuals/r-release/R-lang.html#Classes
 str(ref.d) #structure of the columns
 
-# two factors nad the  remainder are integers
-summary(ref.d$Year)
+# Renaming the names in of certain countries for brevity
+# http://www.cookbook-r.com/Manipulating_data/Renaming_levels_of_a_factor/
+library(plyr) #load the plyr package
+levels(ref.d$Country)<-revalue(ref.d$Country, 
+	c("Bolivia (Plurinational State of)"="Bolivia",
+	"China, Hong Kong SAR"="Hong Kong",
+	"China, Macao SAR"="Macao",
+	"Iran (Islamic Rep. of)"="Iran",
+	"Micronesia (Federated States of)"="Micronesia",
+	"Serbia and Kosovo (S/RES/1244 (1999))"="Serbia & Kosovo",
+	"Venezuela (Bolivarian Republic of)"="Venezuela",
+	"Various/Unknown"="Unknown")
+	)
+
+levels(ref.d$Country_Origin)<-revalue(ref.d$Country_Origin,
+		c("Bolivia (Plurinational State of)"="Bolivia",
+	"China, Hong Kong SAR"="Hong Kong",
+	"China, Macao SAR"="Macao",
+	"Iran (Islamic Rep. of)"="Iran",
+	"Micronesia (Federated States of)"="Micronesia",
+	"Serbia and Kosovo (S/RES/1244 (1999))"="Serbia & Kosovo",
+	"Venezuela (Bolivarian Republic of)"="Venezuela",
+	"Various/Unknown"="Unknown")
+	)
+levels(ref.d$Country)
+levels(ref.d$Country_Origin)
+str(ref.d)
+
+## ^ above replaces what was done before but erorrs occur when I convert from factor to string type
+# ref.d$Country_Origin[ref.d$Country_Origin=="Various/Unknown"] <- "Unknown" #change the "Various/Unknown" to a new string label "UNKNOWN" in the country of origin column
+# ref.d$Country[ref.d$Country=="Various/Unknown"] <- "Unknown" #do the same for the countries
+
+# two factors nad the remainder are integers
+summary(ref.d$Year);class(ref.d$Year);range(ref.d$Year)
 ref.d$Country <- as.character(ref.d$Country) #convert from factor to string
 ref.d$Country_Origin <-as.character(ref.d$Country_Origin) #convert from factor to string
 
@@ -66,37 +98,59 @@ for(i in 2:length(names(ref.d))){ # "2"-ignores the first column (we want to kee
 	}
 }
 
-ref.d$Country_Origin[ref.d$Country_Origin=="Various/Unknown"] <- "UNKNOWN" #change the "Various/Unknown" to a new string label "UNKNOWN" in the country of origin column
-ref.d$Country[ref.d$Country=="Various/Unknown"] <- "UNKNOWN" #do the same for the countries
+ref.d$Country_Origin[ref.d$Country_Origin=="Various/Unknown"] <- "Unknown" #change the "Various/Unknown" to a new string label "UNKNOWN" in the country of origin column
+ref.d$Country[ref.d$Country=="Various/Unknown"] <- "Unknown" #do the same for the countries
+
+
+# > table(ref.d$Country)
+
+               # Australia                  Austria                  Belgium 
+                     # 102                     4046                     3989 
+                 # Burundi                   Canada   Dem. Rep. of the Congo 
+                     # 164                     4469                      952 
+                 # Denmark                   France                  Germany 
+                    # 5710                     6500                     3924 
+                  # Greece                Hong Kong                    Italy 
+                    # 5175                      255                     5411 
+              # Luxembourg                  Morocco              Netherlands 
+                    # 6955                     8303                     7052 
+                  # Norway                    Spain                   Sweden 
+                    # 4882                     2170                     5172 
+             # Switzerland                  Tunisia                   Turkey 
+                    # 2656                     1123                     6932 
+          # United Kingdom United States of America                  Unknown 
+                    # 5574                     8538                     3692 
 
 table(ref.d$Country) #check the above changes
 table(ref.d$Country_Origin) #check the above changes
 
-
 clist<-sort(unique(ref.d$Country)) #alphabetical list of countries in Country
+which(ref.d$Country=="Bolivia (Plurinational State of)")
+
+ref.d[ref.d$Country=="Bolivia (Plurinational State of)",]
+
 or.clist<-sort(unique(ref.d$Country_Origin)) #alphabetical list of countries in Country_Origin
+or.clist
+
 
 # We want to if there are any differences between these two vectors of country names
 # http://stackoverflow.com/questions/9162134/comparing-character-vectors-in-r-to-find-unique-and-or-missing-values
 clist[!clist %in% or.clist] #which countries from clist are not in or.clist
  # this means that these countries either don't have data for refugees or there are no refugees in these countries
  # this means that these countries have not produced refugees or there is no data on refugees from these countries
-setdiff(countries,or.countries) #also accomplishes the same as line 81
+setdiff(clist,or.clist) #also accomplishes the same as line 81
 
 or.clist[!or.clist %in% clist] #which countries from or.clist are not in clist
  # this means that these are ONLY countries of origin and either there is no data that refugees seltted or there are no refugees in these countries
  # this means that these countries (ie. NK) have only produced refugees (not taken in any refugees) or there is no data on this.
-setdiff(or.countries,countries) #also accomplishes the same as line 86
+setdiff(or.clist,clist) #also accomplishes the same as line 86
 
 ref.d[which(ref.d$Country=="Bonaire"),] #retrieves rows that match the criteria: country Bonaire
 head(ref.d[which(ref.d$Country_Origin=="Dem. People's Rep. of Korea"),]) #retrievs the rows that match the criteria: country of origin as NK
 
+apply(ref.d[1:10,1:10],2,function(x) paste0(x,"asdf")) #adding asdf text to the selection rows
 
-apply(ref.d,2,function(x) ifelse(class(x)=="factor",as.numeric(x)))
-apply(ref.d[1:10,1:10],2,function(x) paste0(x,"asdf"))
-
-
-# Make a wordcloud to qualitatively visualize the countries that have taken in refugees
+# Make a wordcloud to qualitatively visualize... which countries have the most reports, that have taken in refugees
 library(wordcloud) #https://www.r-bloggers.com/word-cloud-in-r/
 countries<-sort(unique(ref.d$Country),decreasing=F)
 countries
@@ -125,5 +179,36 @@ wordcloud(or.countries,
 	random.order=FALSE,
 	colors=pal2)
 
-for (i in )
+?aggregate
+# Sum the Totals by Year, Country, Country of Origin
+names(ref.d) #http://stackoverflow.com/questions/15933968/r-programming-sum-elements-of-rows-with-common-values?rq=1
+year.tot<-aggregate(cbind(Total)~Year,data=ref.d,FUN=sum)
+or.country.tot<-aggregate(cbind(Total)~Country_Origin,data=ref.d,FUN=sum)
 
+pal3 <- c("#274c56", #http://tools.medialab.sciences-po.fr/iwanthue/
+"#664c47",
+"#4e5c48",
+"#595668",
+"#395e68",
+"#516964",
+"#6b6454",
+"#58737f",
+"#846b6b",
+"#807288",
+"#758997",
+"#7e9283",
+"#a79486",
+"#aa95a2",
+"#8ba7b4")
+
+# Install more Fonts
+install.packages("extrafont")
+library(extrafont)
+loadfonts()
+wordcloud(or.country.tot[,1],
+	or.country.tot[,2],
+	scale=c(3,.5),
+	min.freq=100,
+	family="Garamond", font=2,
+	random.order=F,
+	colors=rev(pal3))
